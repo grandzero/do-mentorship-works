1276c31f71ed0cda6ee9a947b95031ba4e0d7352
grandzero <bayramutkuuzunlar@gmail.com>
2022-03-01 16:20:05 +0300

    create storage variables

--- cryptofund/contracts/CryptoFund.sol
+++ cryptofund/contracts/CryptoFund.sol
@@ -3,15 +3,19 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "solidity-linked-list/contracts/StructuredLinkedList.sol";
+import "./lib/EnumerableMap.sol";
 
 contract CryptoFund {
-    using StructuredLinkedList for StructuredLinkedList.List;
-    address public owner;
-
     struct Investor {
         uint256 totalFund;
         StructuredLinkedList.List investmentsList;
     }
+    using StructuredLinkedList for StructuredLinkedList.List;
+    using EnumerableMap for EnumerableMap.AddressToUintMap;
+
+    EnumerableMap.AddressToUintMap private startups;
+    Investor[] private investors;
+    address public owner;
 
     constructor() {
         owner = msg.sender;
