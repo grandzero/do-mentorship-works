aa94de6dec05e44c0fb61744d3a9e967a660d3fd
grandzero <bayramutkuuzunlar@gmail.com>
2022-03-05 22:56:38 +0300

    remove pausable from erc721a

--- optimization/contracts/ERC721ATest.sol
+++ optimization/contracts/ERC721ATest.sol
@@ -4,30 +4,10 @@ pragma solidity ^0.8.4;
 import "./lib/ERC721A.sol";
 
 contract ERC721Test is ERC721A {
-    uint256 public tokenCounter;
     constructor() ERC721("ERC721Test", "ET721") {}
-    
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
-    }
-
+   
     function claim() public {
-        _safeMint(msg.sender, tokenCounter++);
+        _safeMint(msg.sender, 1);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
 }
